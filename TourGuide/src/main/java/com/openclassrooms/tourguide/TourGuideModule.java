3a45d023a8b2c3dbaa397c984196819a22d653a4
alcan <alcan@example.com>
2026-02-22 12:22:14 +0100

    bean redondant RewardsService de TourGuideModule - S’appuie sur @Service + injection par constructeur pour RewardsService
    - beans conservés pour les dépendances externes (GpsUtil, RewardCentral)

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideModule.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideModule.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 import gpsUtil.GpsUtil;
 import rewardCentral.RewardCentral;
-import com.openclassrooms.tourguide.service.RewardsService;
 
 @Configuration
 public class TourGuideModule {
@@ -15,10 +14,6 @@ public class TourGuideModule {
 		return new GpsUtil();
 	}
 	
-	@Bean
-	public RewardsService getRewardsService() {
-		return new RewardsService(getGpsUtil(), getRewardCentral());
-	}
 	
 	@Bean
 	public RewardCentral getRewardCentral() {
