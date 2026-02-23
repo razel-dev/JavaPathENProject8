df51cf70b91260665e9ebade4305e9afe3ebd105
alcan <alcan@example.com>
2026-02-23 09:50:52 +0100

    Activation @EnableCaching pour brancher Spring Cache.

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
@@ -2,8 +2,10 @@ package com.openclassrooms.tourguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TourguideApplication {
 
 	public static void main(String[] args) {
